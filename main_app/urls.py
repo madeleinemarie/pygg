892522916bf596b3652b8a374bfcf32a4ef83c3a
madeleinemarie <maddy.vandenheuvel@gmail.com>
2020-06-22 16:36:54 -0700

    Create bill detail view, links from index

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('accounts/signup/', views.signup, name='signup'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('bills/', views.BillList.as_view(), name='bills'),
+    path('bills/<int:pk>', views.BillDetail.as_view(), name='bill_detail'),
     path('bills/create/', views.BillCreate.as_view(), name='bills_create'),
     path('bills/<int:pk>/update/', views.BillUpdate.as_view(), name='bills_update'),
     path('bills/<int:pk>/delete/', views.BillDelete.as_view(), name='bills_delete'),
