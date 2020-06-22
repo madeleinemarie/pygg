45147f558e23c16677121d5da61bc66ae6d15333
madeleinemarie <maddy.vandenheuvel@gmail.com>
2020-06-21 20:29:28 -0700

    Change redirect url after login to dashboard

--- pygg/settings.py
+++ pygg/settings.py
@@ -119,5 +119,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-LOGIN_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = '/dashboard/'
 LOGOUT_REDIRECT_URL = '/'
