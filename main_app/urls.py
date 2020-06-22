d1c54281ecad8782a7e4a33a9a8996ceaa9d4a83
madeleinemarie <maddy.vandenheuvel@gmail.com>
2020-06-21 20:29:13 -0700

    Add dashboard and bills patterns routes

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('accounts/signup/', views.signup, name='signup')
+    path('accounts/signup/', views.signup, name='signup'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('bills/', views.BillList.as_view(), name='bills'),
+    path('bills/create/', views.BillCreate.as_view(), name='bills_create'),
 ]
