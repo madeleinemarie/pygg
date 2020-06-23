ec9550fa9404e61009ecb28f4b6e2f4117a39451
madeleinemarie <maddy.vandenheuvel@gmail.com>
2020-06-23 16:06:45 -0700

    Fix date calculation logic, add custom template tag for applying css class based on due/overdue status

--- main_app/templatetags/date_extras.py
+++ main_app/templatetags/date_extras.py
@@ -13,15 +13,29 @@ def days_until(target_date):
         delta = target_date.date() - today
     if delta.days > 1:
         return f"Due in {delta.days} days"
-    elif delta.days < 1 and delta.days > 0:
+    elif delta.days == 1:
         return 'Due tomorrow'
     elif delta.days == 0:
         return 'Due today'
     else:
         return 'Overdue'
 
-# @register.filter(name='daysstyle')
-# def days_style
+@register.filter(name='daysstyle')
+def days_style(target_date):
+    today = datetime.date.today()
+    if isinstance(target_date, datetime.date):
+        delta = target_date - today
+    else:
+        delta = target_date.date() - today
+    if delta.days > 1:
+        return 'due-later'
+    elif delta.days == 1:
+        return 'due-tomorrow'
+    elif delta.days == 0:
+        return 'due-today'
+    else:
+        return 'overdue'
+
 
 @register.filter(name='justdays', expects_localtime=True)
 def just_days(target_date):
@@ -32,7 +46,7 @@ def just_days(target_date):
         delta = target_date.date() - today
     if delta.days > 1:
         return f"{delta.days} days"
-    elif delta.days < 1 and delta.days > 0:
+    elif delta.days == 1:
         return 'Tomorrow'
     elif delta.days == 0:
         return 'Today'
