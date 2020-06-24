397778af4b51051fef1079365ecd6ddebe2c9568
Alex <alex.udarbe@gmail.com>
2020-06-24 10:14:12 -0700

    UI updates

--- main_app/static/js/main.js
+++ main_app/static/js/main.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', function() {
     $('tr#category-row td:contains("Utilities")').closest('tr').css('background-color','mediumpurple');
     $('tr#category-row td:contains("Insurance")').closest('tr').css('background-color','mediumaquamarine');
     $('tr#category-row td:contains("Credit Cards")').closest('tr').css('background-color','steelblue');
-    $('tr#category-row td:contains("Loans")').closest('tr').css('background-color','gainsboro');
+    $('tr#category-row td:contains("Loans")').closest('tr').css('background-color','wheat');
 
     $("td#category-color[data-category='Miscellaneous']").css('background-color','lightsalmon')
     $("td#category-color[data-category='Mortgage']").css('background-color','hotpink')
@@ -15,7 +15,7 @@ document.addEventListener('DOMContentLoaded', function() {
     $("td#category-color[data-category='Utilities']").css('background-color','mediumpurple')
     $("td#category-color[data-category='Insurance']").css('background-color','mediumaquamarine')
     $("td#category-color[data-category='Credit Cards']").css('background-color','steelblue')
-    $("td#category-color[data-category='Loans']").css('background-color','gainsboro')
+    $("td#category-color[data-category='Loans']").css('background-color','wheat')
 
     $("div#category-color[data-category='Miscellaneous']").css('background-color','lightsalmon')
     $("div#category-color[data-category='Mortgage']").css('background-color','hotpink')
@@ -24,5 +24,5 @@ document.addEventListener('DOMContentLoaded', function() {
     $("div#category-color[data-category='Utilities']").css('background-color','mediumpurple')
     $("div#category-color[data-category='Insurance']").css('background-color','mediumaquamarine')
     $("div#category-color[data-category='Credit Cards']").css('background-color','steelblue')
-    $("div#category-color[data-category='Loans']").css('background-color','gainsboro')
+    $("div#category-color[data-category='Loans']").css('background-color','wheat')
 });
